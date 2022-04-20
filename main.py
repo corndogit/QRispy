ffaf84458577a2e4455b1b5d6d8e799d5789f88a
Nathan <101812777+corndogit@users.noreply.github.com>
2022-04-20 15:50:23 +0100

    added QR decoding

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
-import qrcode
 import sys
+import qrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
 
 
 def make_qr_code(data: str):
-    """| Turns string of data into a QR code in PNG format"""
+    """| Turns string object into a QR code in PNG format"""
     image = qrcode.make(data)
     return image
 
 
+def decode_qr_code(data):
+    """| Finds QR code in image and decodes it using pyzbar library"""
+    decode_qr = decode(Image.open(data))
+    return decode_qr
+
+
 def main():
-    """| Usage in terminal: main.py <string> <output_img>"""
+    """| Usage in terminal: main.py <encode/decode> <input> <output>"""
     try:
-        script, data_input, filename = sys.argv
-        make_qr_code(data_input).save(filename)
-        print(f"Generated QR code, saved to {filename}")
+        script, task, data_input = sys.argv
+        if task.lower() == 'encode':
+            make_qr_code(data_input).save('output.png')
+            print(f"Generated QR code, saved to output.png")
+        elif task.lower() == 'decode':
+            print(decode_qr_code(data_input))
+        else:
+            print("Invalid task  - please use 'encode' or 'decode'")
     except ValueError:
-        data_input = input("Enter text to encode:\n> ")
-        filename = input("Enter name for output file:\n> ")
-        make_qr_code(data_input).save(filename)
-        print(f"Generated QR code, saved to {filename}")
+        print("Incorrect arguments (Usage: main.py <encode/decode> <input> <output>)")
 
 
 if __name__ == '__main__':
