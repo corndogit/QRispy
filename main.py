5aaba4a7fe469689aa6ff72a411ea04475e59edf
Nathan <101812777+corndogit@users.noreply.github.com>
2022-04-20 22:33:58 +0100

    begin GUI implementation

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-import sys
-import qrcode
-from pyzbar.pyzbar import decode
+import tkinter as tk
 from PIL import Image
-
-
-def make_qr_code(data: str):
-    """| Turns string object into a QR code in PNG format"""
-    image = qrcode.make(data)
-    return image
-
-
-def decode_qr_code(data):
-    """| Finds QR code in image and decodes it using pyzbar library"""
-    decode_qr = decode(Image.open(data))
-    return decode_qr
+from qr_codec import make_qr_code, decode_qr_code
 
 
 def main():
-    """| Usage in terminal: main.py <encode/decode> <input> <output>"""
-    try:
-        script, task, data_input = sys.argv
-        if task.lower() == 'encode':
-            make_qr_code(data_input).save('output.png')
-            print("Generated QR code, saved to output.png")
-        elif task.lower() == 'decode':
-            raw_data = decode_qr_code(data_input)  # list object, example in README.md
-            print(raw_data[0].data.decode('utf-8'))
-        else:
-            print("Invalid task  - please use 'encode' or 'decode'")
-    except ValueError:
-        print("Incorrect arguments (Usage: main.py <encode/decode> <input> <output>)")
+    # Window
+    window = tk.Tk()
+    window.title("QRispy")
+    canvas = tk.Canvas(window, width=800, height=600)
+    canvas.grid(columnspan=4)
+
+    window.mainloop()
 
 
 if __name__ == '__main__':
---
+++ qr_codec.py
@@ -0,0 +1,35 @@
+import sys
+import qrcode
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+
+def make_qr_code(data: str):
+    """| Turns string object into a QR code in PNG format"""
+    image = qrcode.make(data)
+    return image.save('output.png')
+
+
+def decode_qr_code(data):
+    """| Finds QR code in image and decodes it using pyzbar library"""
+    decode_qr = decode(Image.open(data))  # PIL.PngImagePlugin.PngImageFile
+    return decode_qr[0].data.decode('utf-8')
+
+
+def main():
+    """| Usage in terminal: main.py <encode/decode> <input> <output>"""
+    try:
+        script, task, data_input = sys.argv
+        if task.lower() == 'encode':
+            make_qr_code(data_input)
+            print("Generated QR code, saved to output.png")
+        elif task.lower() == 'decode':
+            print(decode_qr_code(data_input))
+        else:
+            print("Invalid task  - please use 'encode' or 'decode'")
+    except ValueError:
+        print("Incorrect arguments (Usage: main.py <encode/decode> <input> <output>)")
+
+
+if __name__ == '__main__':
+    main()
