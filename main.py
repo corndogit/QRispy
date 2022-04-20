6f975e981ea1ab66895cad72eca99c8c632a64cc
Nathan <101812777+corndogit@users.noreply.github.com>
2022-04-20 12:01:18 +0100

    edit main(): try argvs, otherwise take manual inputs

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ import qrcode
 import sys
 
 
-def make_qr_code(data):
-    """Turns data into a QR code in PNG format"""
+def make_qr_code(data: str):
+    """| Turns string of data into a QR code in PNG format"""
     image = qrcode.make(data)
     return image
 
 
-if __name__ == '__main__':
+def main():
+    """| Usage in terminal: main.py <string> <output_img>"""
     try:
         script, data_input, filename = sys.argv
         make_qr_code(data_input).save(filename)
         print(f"Generated QR code, saved to {filename}")
     except ValueError:
-        print("Not enough arguments supplied (usage: main.py <string> <output_img>)")
+        data_input = input("Enter text to encode:\n> ")
+        filename = input("Enter name for output file:\n> ")
+        make_qr_code(data_input).save(filename)
+        print(f"Generated QR code, saved to {filename}")
+
+
+if __name__ == '__main__':
+    main()
