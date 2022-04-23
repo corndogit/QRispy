afa0135ca88768d591984aa5e3d84e1646539ef4
Nathan <101812777+corndogit@users.noreply.github.com>
2022-04-23 23:48:33 +0100

    window layout for encode

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
-from PIL import Image
+from tkinter import ttk
+from PIL import Image, ImageTk
 from qr_codec import make_qr_code, decode_qr_code
 
 
@@ -7,8 +8,37 @@ def main():
     # Window
     window = tk.Tk()
     window.title("QRispy")
-    canvas = tk.Canvas(window, width=800, height=600)
-    canvas.grid(columnspan=4)
+    canvas = tk.Canvas(window, width=700, height=350)
+    canvas.grid(columnspan=2, rowspan=2)
+
+    # Title
+    title = ttk.Label(window)
+    title.config(text="Encode:")
+    title.grid(column=0, row=0, sticky='NW', padx=25, pady=10)
+
+    # Text input
+    text_input = tk.Entry(window, width=48)
+    text_input.grid(column=0, row=0, sticky='NW', padx=25, pady=30)
+    convert_input = ttk.Button(window, text="Convert")
+    convert_input.grid(column=0, row=0, sticky='NE', pady=50, padx=68)
+    browse_input = ttk.Button(window, text="Browse")
+    browse_input.grid(column=0, row=0, sticky='NE', pady=75, padx=68)
+
+    # QR code display
+    img_qr = Image.open('output.png').resize((270, 270))
+    img_qr = ImageTk.PhotoImage(img_qr)
+    img_qr_label = ttk.Label(image=img_qr)
+    img_qr_label.image = img_qr
+    img_qr_label.grid(column=1, row=0)
+
+    # radio buttons
+    mode = tk.StringVar()
+    subtitle = ttk.Label(window, text="Mode:")
+    subtitle.grid(column=0, row=1, sticky='NW', padx=25)
+    encode = ttk.Radiobutton(window, text="Encode", value='encode')
+    decode = ttk.Radiobutton(window, text="Decode", value='decode')
+    encode.grid(column=0, row=1, sticky='NW', padx=25, pady=25)
+    decode.grid(column=0, row=1, sticky='NW', padx=125, pady=25)
 
     window.mainloop()
 
