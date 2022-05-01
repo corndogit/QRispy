96c013b63b6c63bb7104c423396502c8bfe7eca9
Nathan <101812777+corndogit@users.noreply.github.com>
2022-05-01 10:46:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,12 @@ def main():
     text_input.grid(column=0, row=0, sticky='NW', padx=25, pady=30)
     convert_input = ttk.Button(window, text="Convert")
     convert_input.grid(column=0, row=0, sticky='NE', pady=50, padx=68)
-    browse_input = ttk.Button(window, text="Browse")
+    browse_input = ttk.Button(window, text="Browse...", command=lambda: fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=(('text files', '*.txt'),
+                   ('All files', '*.*'))
+    ))
     browse_input.grid(column=0, row=0, sticky='NE', pady=75, padx=68)
 
     # QR code display
@@ -33,13 +38,18 @@ def main():
     img_qr_label.grid(column=1, row=0)
 
     # radio buttons
-    # mode = tk.StringVar()
     # subtitle = ttk.Label(window, text="Mode:")
     # subtitle.grid(column=0, row=1, sticky='NW', padx=25)
-    # encode = ttk.Radiobutton(window, text="Encode", value='encode')
-    # decode = ttk.Radiobutton(window, text="Decode", value='decode')
-    # encode.grid(column=0, row=1, sticky='NW', padx=25, pady=25)
-    # decode.grid(column=0, row=1, sticky='NW', padx=125, pady=25)
+    # selected_mode = tk.StringVar()
+    # modes = (('Encode', 'encode'), ('Decode', 'decode'))
+    # x_padding = 25
+    # for m in modes:
+    #     r = ttk.Radiobutton(window,
+    #                         text=m[0],
+    #                         value=m[1],
+    #                         variable=selected_mode)
+    #     r.grid(column=0, row=1, sticky='NW', padx=x_padding, pady=25)
+    #     x_padding += 100
 
     window.mainloop()
 
