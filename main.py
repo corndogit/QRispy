c7a63bbaecd4a95691f014885a2f785858e4f25e
Nathan <101812777+corndogit@users.noreply.github.com>
2022-05-01 13:17:38 +0100

    Implemented string to QR code conversion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import filedialog as fd
+import tkinter.messagebox
 from PIL import Image, ImageTk
 from qr_codec import make_qr_code, decode_qr_code
 
@@ -17,27 +19,44 @@ def main():
     title.config(text="Encode:")
     title.grid(column=0, row=0, sticky='NW', padx=25, pady=10)
 
+    # QR code display
+    img_qr = ImageTk.PhotoImage(Image.open('main_logo.png'))
+    img_qr_label = ttk.Label(window, image=img_qr)
+    img_qr_label.grid(column=1, row=0)
+
     # Text input
     text_input = tk.Entry(window, width=48)
     text_input.grid(column=0, row=0, sticky='NW', padx=25, pady=30)
-    convert_input = ttk.Button(window, text="Convert")
+    convert_input = ttk.Button(window, text="Convert", command=lambda: get_text_to_encode(text_input.get()))
     convert_input.grid(column=0, row=0, sticky='NE', pady=50, padx=68)
-    browse_input = ttk.Button(window, text="Browse...", command=lambda: fd.askopenfilename(
-        title='Open a file',
-        initialdir='/',
-        filetypes=(('text files', '*.txt'),
-                   ('All files', '*.*'))
-    ))
+    browse_input = ttk.Button(window, text="Convert from file...", command=lambda: get_file_to_encode())
+
+    def get_text_to_encode(text):
+        make_qr_code(text).save('output.png')
+        generated_qr = ImageTk.PhotoImage(Image.open('output.png'))
+        img_qr_label.configure(image=generated_qr)
+        # tk.messagebox.showinfo(title='Success!', message='Your QR code was saved as output.png')
+
+    def get_file_to_encode():
+        file = fd.askopenfilename(
+            title='Open a file',
+            initialdir='/',
+            filetypes=(('Text to encode', '*.txt'),
+                       ('All files', '*.*'))
+        )
+        if file:
+            try:
+                read_file = "".join(open(file, 'r').readlines())
+                make_qr_code(read_file).save('output.png')
+                generated_qr = ImageTk.PhotoImage(Image.open('output.png'))
+                img_qr_label.configure(image=generated_qr)
+                # tk.messagebox.showinfo(title='Success!', message='Your QR code was saved as output.png')
+            except UnicodeDecodeError:
+                tk.messagebox.showerror(title='Error', message='Unable to read file (please use only plaintext files)')
+
     browse_input.grid(column=0, row=0, sticky='NE', pady=75, padx=68)
 
-    # QR code display
-    img_qr = Image.open('main_logo.png').resize((270, 270))
-    img_qr = ImageTk.PhotoImage(img_qr)
-    img_qr_label = ttk.Label(image=img_qr)
-    img_qr_label.image = img_qr
-    img_qr_label.grid(column=1, row=0)
-
-    # radio buttons
+    # radio buttons - for switching between encode and decode layout
     # subtitle = ttk.Label(window, text="Mode:")
     # subtitle.grid(column=0, row=1, sticky='NW', padx=25)
     # selected_mode = tk.StringVar()
--- qr_codec.py
+++ qr_codec.py
@@ -7,7 +7,7 @@ from PIL import Image
 def make_qr_code(data: str):
     """| Turns string object into a QR code in PNG format"""
     image = qrcode.make(data)
-    return image.save('output.png')
+    return image
 
 
 def decode_qr_code(data):
@@ -21,7 +21,7 @@ def main():
     try:
         script, task, data_input = sys.argv
         if task.lower() == 'encode':
-            make_qr_code(data_input)
+            make_qr_code(data_input).save('output.png')
             print("Generated QR code, saved to output.png")
         elif task.lower() == 'decode':
             print(decode_qr_code(data_input))
